d010fc8fb11c09a13e91361c51bce756f173809d
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-06 20:02:00 +0545

    feat: absolute named exports for pages created

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home/Home.jsx";
+export { default as AboutUs } from "./AboutUs/AboutUs.jsx";
+export { default as Services } from "./Services/Services.jsx";
+export { default as ContactUs } from "./ContactUs/ContactUs.jsx";
