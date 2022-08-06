1af4cefdfc62d2470fd58fb0efefdd0c4e03da65
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-06 19:56:35 +0545

    feat: Routes added for each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import { Home, AboutUs, Services, ContactUs } from "@pages";
+
 const App = () => {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+      </Routes>
     </div>
   );
 };
