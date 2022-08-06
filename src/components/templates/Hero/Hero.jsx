66852d9c08c67db710d63a25fa6ab708f3d98ec4
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-06 22:17:34 +0545

    feat: abstract components added to the markup

--- src/components/templates/Hero/Hero.jsx
+++ src/components/templates/Hero/Hero.jsx
@@ -1,16 +1,13 @@
-import star from "@assets/images/star.svg";
-import arrow from "@assets/images/arrow-direction.svg";
+import { HeroRightColumn } from "@components/modules";
+import { HeroAsideImg, HeroLeftColumn } from "@components/elements";
 import "./Hero.scss";
 
 const Hero = () => {
   return (
     <section className="hero">
-      <aside className="hero__aside">
-        <figure className="hero__img-wrapper">
-          <img src={star} alt="Star" className="hero__star" />
-          <img src={arrow} alt="Arrow" className="hero__arrow" />
-        </figure>
-      </aside>
+      <HeroAsideImg />
+      <HeroLeftColumn />
+      <HeroRightColumn />
     </section>
   );
 };
