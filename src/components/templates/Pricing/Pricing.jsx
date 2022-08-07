074f620776c972c287d9083c3b7373637f27b299
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-07 14:12:12 +0545

    feat: markups for Pricing component added

--- src/components/templates/Pricing/Pricing.jsx
+++ src/components/templates/Pricing/Pricing.jsx
@@ -1,4 +1,26 @@
+import { PricingPlan, SectionHeader } from "@components/elements";
+import { pricingData } from "@data/pricing-data";
+import "./Pricing.scss";
+
 const Pricing = () => {
-  return <div>Pricing</div>;
+  return (
+    <section className="pricing" aria-labelledby="pricing-header">
+      <SectionHeader
+        id="pricing-header"
+        title="Our Plans Scale With Your Product"
+        subtitle="Choose the best pricing plans as per your requirement. We will provide you with the most suitable plan for your product"
+      />
+      <section className="pricing__content">
+        {pricingData.map((price) => (
+          <PricingPlan
+            key={price.id}
+            title={price.title}
+            price={price.price}
+            features={price.features}
+          />
+        ))}
+      </section>
+    </section>
+  );
 };
 export default Pricing;
