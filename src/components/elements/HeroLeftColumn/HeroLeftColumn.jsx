2fd0b7860a9e1bf9b121dc3c20662cce525755af
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-07 14:07:57 +0545

    feat: markup for left column in hero section added

--- src/components/elements/HeroLeftColumn/HeroLeftColumn.jsx
+++ src/components/elements/HeroLeftColumn/HeroLeftColumn.jsx
@@ -2,17 +2,17 @@ import "./HeroLeftColumn.scss";
 
 const HeroLeftColumn = () => {
   return (
-    <section aria-labelledby="hero-header" class="left-col">
-      <h1 id="hero-header" class="left-col__header">
+    <section aria-labelledby="hero-header" className="left-col">
+      <h1 id="hero-header" className="left-col__header">
         Tell a better brand story
       </h1>
-      <p class="left-col__subheader">
+      <p className="left-col__subheader">
         Warrant present garrets limited cordial in inquiry to. Supported me
         sweetness behaviour shameless excellent so arranging.
       </p>
-      <div class="left-col__btn">
-        <button class="btn btn__primary">Get Started</button>
-        <button class="btn btn__secondary">Contact Us</button>
+      <div className="left-col__btn">
+        <button className="btn btn__primary">Get Started</button>
+        <button className="btn btn__secondary">Contact Us</button>
       </div>
     </section>
   );
