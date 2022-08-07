32fd27fff1eb4dbf8b2ab1e7b2e8fb9ed443fdc6
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-07 14:11:45 +0545

    feat: markup for our services section added

--- src/components/templates/OurServices/OurServices.jsx
+++ src/components/templates/OurServices/OurServices.jsx
@@ -1,4 +1,27 @@
+import { SectionHeader, ServicesCard } from "@components/elements";
+import { servicesData } from "@data/services-data";
+import "./OurServices.scss";
+
 const OurServices = () => {
-  return <div>OurServices</div>;
+  return (
+    <section className="services" aria-labelledby="services-header">
+      <SectionHeader
+        id="services-header"
+        title="What We Offer?"
+        subtitle="In virtual space through communication platforms. Durable relations that extend beyond immediate genealogical ties."
+      />
+      <section className="services__content">
+        {servicesData.map((service) => (
+          <ServicesCard
+            key={service.id}
+            Icon={service.icon}
+            title={service.title}
+            desc={service.description}
+          />
+        ))}
+      </section>
+    </section>
+  );
 };
+
 export default OurServices;
