61dffb038609e19f2f1885c64509b3e850af775d
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-07 16:41:47 +0545

    feat: markup added

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,4 +1,36 @@
+import "./AboutUs.scss";
+
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <section className="about">
+      <h1 className="about__header">About Us</h1>
+      <p className="about__description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Id interdum velit
+        laoreet id donec ultrices tincidunt arcu non. Tellus cras adipiscing
+        enim eu turpis egestas pretium aenean. Aliquam vestibulum morbi blandit
+        cursus risus. Erat pellentesque adipiscing commodo elit at. Varius quam
+        quisque id diam vel quam elementum. Sit amet dictum sit amet. Fermentum
+        dui faucibus in ornare quam viverra. Nibh tortor id aliquet lectus
+        proin. Ultrices dui sapien eget mi. Euismod quis viverra nibh cras
+        pulvinar. Et netus et malesuada fames ac turpis. Pulvinar elementum
+        integer enim neque volutpat ac. Sit amet aliquam id diam maecenas
+        ultricies mi.
+        <br />
+        <br />
+        Erat velit scelerisque in dictum non consectetur a erat. Eu non diam
+        phasellus vestibulum lorem sed. Bibendum neque egestas congue quisque
+        egestas diam. Suspendisse sed nisi lacus sed viverra tellus. Viverra
+        adipiscing at in tellus integer feugiat scelerisque varius. Neque
+        sodales ut etiam sit amet. Libero id faucibus nisl tincidunt eget.
+        Tempor nec feugiat nisl pretium fusce id velit ut. Enim neque volutpat
+        ac tincidunt vitae semper quis. Amet mauris commodo quis imperdiet massa
+        tincidunt nunc pulvinar sapien. Leo integer malesuada nunc vel.
+        Vulputate enim nulla aliquet porttitor. Platea dictumst quisque sagittis
+        purus sit amet volutpat. Scelerisque fermentum dui faucibus in ornare
+        quam viverra orci. Nunc vel risus commodo viverra.
+      </p>
+    </section>
+  );
 };
 export default AboutUs;
