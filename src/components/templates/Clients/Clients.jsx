0ed00df648f611e60843b8ac7377acb694ae77b0
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-07 14:11:11 +0545

    feat: markup for clients section added

--- src/components/templates/Clients/Clients.jsx
+++ src/components/templates/Clients/Clients.jsx
@@ -1,4 +1,29 @@
+import planDay from "@assets/images/planday.svg";
+import umbraco from "@assets/images/umbraco.svg";
+import pearlFisher from "@assets/images/pearlfisher.svg";
+import brightPearl from "@assets/images/brightpearl.svg";
+import "./Clients.scss";
+
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <section className="clients">
+      <figure className="clients__logo">
+        <img src={planDay} alt="Planday Logo" />
+      </figure>
+      <figure className="clients__logo">
+        <img src={umbraco} alt="Umbraco Logo" />
+      </figure>
+      <figure className="clients__logo">
+        <img src={pearlFisher} alt="Pearl Fisher Logo" />
+      </figure>
+      <figure className="clients__logo">
+        <img src={brightPearl} alt="Bright Pearl Logo" />
+      </figure>
+      <figure className="clients__logo">
+        <img src={planDay} alt="Planday Logo" />
+      </figure>
+    </section>
+  );
 };
+
 export default Clients;
