0ff98e58da81708c1b7f1ffe9655dafd70dd10c5
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-07 16:17:22 +0545

    refactor: unused element tag removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
+import { AboutUs, ContactUs, Home, Services } from "@pages";
 import { Route, Routes } from "react-router-dom";
-import { Home, AboutUs, Services, ContactUs } from "@pages";
 
 const App = () => {
   return (
-    <div className="App">
+    <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact-us" element={<ContactUs />} />
       </Routes>
-    </div>
+    </>
   );
 };
 
