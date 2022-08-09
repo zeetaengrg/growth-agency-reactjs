e72904a4efe7820d0293b02b3fa0ebb82187e2e9
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-09 14:11:26 +0545

    feat: markup for navbar mobile menu added

--- src/components/templates/Navbar/Navbar.jsx
+++ src/components/templates/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { CgMenuBoxed, CgCloseR } from "react-icons/cg";
 
 import { NavLinkItem, NavLogo } from "@components/elements";
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const linksItem = ["Home", "About Us", "Services", "Contact Us"];
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const openMenu = () => (
+    <CgMenuBoxed className="nav__menu-btn" onClick={toggleMenu} />
+  );
+
+  const closeMenu = () => (
+    <CgCloseR className="nav__menu-btn" onClick={toggleMenu} />
+  );
 
   return (
     <header className="nav" id="navbar">
@@ -16,9 +32,14 @@ const Navbar = () => {
           ))}
         </ul>
       </nav>
-      <Link to="/login">
-        <button className="nav__login-btn">Login</button>
-      </Link>
+      <section className="nav__right" id="right-navigation-section">
+        <Link to="/login">
+          <button className="nav__login-btn">Login</button>
+        </Link>
+        <figure className="nav__menu">
+          {isOpen ? closeMenu() : openMenu()}
+        </figure>
+      </section>
     </header>
   );
 };
