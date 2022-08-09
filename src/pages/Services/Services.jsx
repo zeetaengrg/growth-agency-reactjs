a9454e6327b10a2fffb83ebcce0320d4d5ec4947
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-09 14:12:41 +0545

    feat: classname selector modified

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -2,9 +2,10 @@ import "./Services.scss";
 
 const Services = () => {
   return (
-    <section className="services">
-      <h1 className="services__header">Our Services</h1>
+    <section className="services-page">
+      <h1 className="services-page__header">Our Services</h1>
     </section>
   );
 };
+
 export default Services;
