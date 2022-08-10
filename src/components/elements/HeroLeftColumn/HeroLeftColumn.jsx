bc5f45e980cf37cf45c64966d9172bda500c619c
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-10 16:49:00 +0545

    feat: markup for button modified

--- src/components/elements/HeroLeftColumn/HeroLeftColumn.jsx
+++ src/components/elements/HeroLeftColumn/HeroLeftColumn.jsx
@@ -2,18 +2,26 @@ import "./HeroLeftColumn.scss";
 
 const HeroLeftColumn = () => {
   return (
-    <section aria-labelledby="hero-header" className="left-col">
+    <section
+      aria-labelledby="hero-header"
+      className="left-col"
+      aria-describedby="hero-subheader"
+    >
       <h1 id="hero-header" className="left-col__header">
         Tell a better brand story
       </h1>
-      <p className="left-col__subheader">
+      <p id="hero-subheader" className="left-col__subheader">
         Warrant present garrets limited cordial in inquiry to. Supported me
         sweetness behaviour shameless excellent so arranging.
       </p>
-      <div className="left-col__btn">
-        <button className="btn btn__primary">Get Started</button>
-        <button className="btn btn__secondary">Contact Us</button>
-      </div>
+      <section aria-label="Button Group" className="left-col__btn">
+        <button className="btn btn--primary" aria-label="Get Started">
+          Get Started
+        </button>
+        <button className="btn btn--secondary" aria-label="Contact Us">
+          Contact Us
+        </button>
+      </section>
     </section>
   );
 };
